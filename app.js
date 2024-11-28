0a8071e48a6f59a652a6dd733aa192a19fbc3955
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2024-11-28 11:21:02 +0000

    update 2

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 require('dotenv').config();
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
@@ -24,6 +26,7 @@ app.post('/login', (req, res) => {
     const password = req.body.password;
     // const { username, password } = req.body;
     if(users[username] === password) {
+        res.cookie('username', username ,{httpOnly: true, secure: true});
         res.status(200).send('Login successful');
         loggedUsers.add(username);
         console.log(`User ${username} logged in`);
@@ -31,6 +34,15 @@ app.post('/login', (req, res) => {
         console.log(loggedUsers);
     } else {
         res.status(401).send('Invalid credentials');
+    }
+});
+
+app.get('/protected', (req, res) => {
+    const username = req.cookies.username;
+    if(loggedUsers.has(username)) {
+        res.send(`Hello, ${username} are logged in.`);
+    } else {
+        res.status(401).send('Unauthorized');
     }
 });
 
